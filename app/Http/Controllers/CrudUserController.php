0c00e73c6ccb7b0496fc9fe414cd2613fa8e7d3a
Nguyen Cao Thanh Tai <tailoveiu@gmail.com>
2024-04-13 03:04:13 +0700

    hoan thanh login

--- app/Http/Controllers/CrudUserController.php
+++ app/Http/Controllers/CrudUserController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CrudUserController extends Controller
 {
-    //
+     /**
+     * Login page
+     */
+    public function login()
+    {
+        return view('crud_user.login');
+    }
+
+    /**
+     * User submit form login
+     */
+    public function authUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('name', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('list')
+                ->withSuccess('Signed in');
+        }
+
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+
 }
