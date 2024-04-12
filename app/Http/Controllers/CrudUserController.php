ab4abcf73d2cfd24471aa13125f6d1cad115e76d
phamnghia521 <pxn200804@gmail.com>
2024-04-13 02:57:27 +0700

    hoan thanh dang ky

--- app/Http/Controllers/CrudUserController.php
+++ app/Http/Controllers/CrudUserController.php
@@ -6,5 +6,34 @@ use Illuminate\Http\Request;
 
 class CrudUserController extends Controller
 {
-    //
+   /**
+     * Registration page
+     */
+    public function createUser()
+    {
+        return view('crud_user.create');
+    }
+
+    /**
+     * User submit form register
+     */
+    public function postUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'password_confirmation' => 'required_with:password|same:password',
+
+        ]);
+
+        $data = $request->all();
+        $check = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+
+        return redirect("login");
+    }
 }
