fcd88fab9813f418b296a201e8dbcfcc067b43cd
CongTri317 <nguyentri3172004@gmail.com>
2024-04-13 04:27:02 +0700

    hoan thanh Update

--- app/Http/Controllers/CrudUserController.php
+++ app/Http/Controllers/CrudUserController.php
@@ -113,4 +113,43 @@ class CrudUserController extends Controller
 
         return view('crud_user.read', ['user' => $user]);
     } 
+    /**
+     * Form update user page
+     */
+    public function updateUser(Request $request)
+    {
+        $user_id = $request->get('id');
+        $user = User::find($user_id);
+
+        return view('crud_user.update', ['user' => $user]);
+    }
+
+    /**
+     * Submit form update user
+     */
+    public function postUpdateUser(Request $request)
+    {
+        $input = $request->all();
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,' . $input['id'],
+            'password' => 'required|min:6',
+            'password_confirmation' => 'required_with:password|same:password',
+
+        ]);
+
+        $user = User::find($input['id']);
+        $user->name = $input['name'];
+        $user->email = $input['email'];
+
+        if (!empty($input['password'])) {
+            $user->password = Hash::make($input['password']);
+        }
+
+        $user->save();
+
+        return redirect("list")->withSuccess('You have signed-in');
+    }
+
 } 
