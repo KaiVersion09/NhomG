a457810b5393f3a1bf8a39bca9506b0d4d065ab4
KaiVersion09 <kaizensu123@gmail.com>
2024-04-24 23:53:04 +0700

    sua list them avatar va phone

--- app/Http/Controllers/CrudUserController.php
+++ app/Http/Controllers/CrudUserController.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Session;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+
 
 class CrudUserController extends Controller
 {
