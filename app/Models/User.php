23b26a8d1063a5e94e11cd2fcec1a0a2695e74ea
Nguyen Cao Thanh Tai <tailoveiu@gmail.com>
2024-05-02 14:50:44 +0700

    update model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Notifications\Notifiable;
+// use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -43,4 +51,30 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+     /**
+     * Relationship one to one
+     * @return HasOne
+     */
+    public function profile(): HasOne
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    /**
+     * Relationship one to many
+     * @return HasMany
+     */
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Posts::class);
+    }
+
+    /**
+     * Relationship many to many
+     * @return HasMany
+     */
+    public function favorities(): BelongsToMany
+    {
+        return $this->belongsToMany(Favorities::class, 'user_favorite', 'user_id', 'favorite_id');
+    }
 }
